597c4e04cf61b84ca3ffc5b862d16c31ea123408
euideok.lee <sharplee@gmail.com>
2021-08-23 22:40:36 +0900

    first commit

--- src/main/java/com/example/responseentityexample/ResponseEntityExampleApplication.java
+++ src/main/java/com/example/responseentityexample/ResponseEntityExampleApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ResponseEntityExampleApplication {
-
 	public static void main(String[] args) {
+		System.out.println("This is a test");
+
 		SpringApplication.run(ResponseEntityExampleApplication.class, args);
 	}
 
