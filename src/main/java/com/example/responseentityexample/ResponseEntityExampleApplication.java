1e46c934b5031f5efbe76617be1419bb82969e38
euideok.lee <sharplee@gmail.com>
2021-08-23 22:30:23 +0900

    first commit

---
+++ src/main/java/com/example/responseentityexample/ResponseEntityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.responseentityexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseEntityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponseEntityExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/responseentityexample/ResponseEntityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.responseentityexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResponseEntityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
